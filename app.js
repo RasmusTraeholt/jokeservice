8a13f6da8f144a257c0b8026013da66f23e57d90
RasmusTraeholt <traeholt@live.dk>
2019-04-03 18:51:00 +0200

    jj

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('we alive?');
+let http = require('http');
+
+let port = process.env.PORT || 8080;
+
+http.createServer(function(request, response) {
+    response.writeHead(200);
+    response.write('We made it!');
+    response.end();
+}).listen(port);
